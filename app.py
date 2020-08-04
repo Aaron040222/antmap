41eafb1d36433ebef2c59d32ec4c6b9019b27bb7
Aaron040222 <aaron040222@gmail.com>
2020-08-04 02:26:18 -0700

    'update'

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from flask import Flask, render_template, jsonify
 import gunicorn
 import psycopg2
 import sys
-
+import geojson 
+import json
 load_dotenv('.env')
 
 app = Flask(__name__)
@@ -24,15 +25,17 @@ def index():
 
 @app.route('/load_data', methods=['GET'])
 def load_data():
-    con = psycopg2.connect("host='ec2-54-243-67-199.compute-1.amazonaws.com' dbname='dkquqt7elufn8' user='pduzwrnjceizrb' password='735761eac08e9ed3b7e26b75e08284c93646930ea3c19a1dc7bf5a9d97371be1'")  
-    cur = con.cursor()
-    cur.execute("""select * from ants_data""")
-    row_headers=[x[0] for x in cur.description]
-    rv = cur.fetchall()
-    json_data=[]
-    for result in rv:
-        json_data.append(dict(zip(row_headers,result)))
-    return jsonify(json_data)
-
+    # con = psycopg2.connect("host='ec2-54-243-67-199.compute-1.amazonaws.com' dbname='dkquqt7elufn8' user='pduzwrnjceizrb' password='735761eac08e9ed3b7e26b75e08284c93646930ea3c19a1dc7bf5a9d97371be1'")  
+    # cur = con.cursor()
+    # cur.execute("""select * from ants_data""")
+    # row_headers=[x[0] for x in cur.description]
+    # rv = cur.fetchall()
+    # json_data=[]
+    # for result in rv:
+    #     json_data.append(dict(zip(row_headers,result)))
+    # return jsonify(json_data)
+    with open ('ants_data.geojson') as f:
+        data = json.load(f)
+    return jsonify(data)
 if __name__ == '__main__':
    app.run(debug=True)
